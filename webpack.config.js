f6dc75c44cc58741b224bc3aadf98922aaf790dc
Mpho Matli <mphojames9@gmail.com>
2026-04-01 20:21:13 +0200

    fix:deploy error

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     publicPath: '/remopdf/',   // ✅ correct for GitHub Pages
     clean: true
   },
