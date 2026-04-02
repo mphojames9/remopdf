7b19f08b64056e012e886fac907819a0af0ba57d
Mpho Matli <mphojames9@gmail.com>
2026-04-02 16:00:20 +0200

    fix: Github pages deploy error

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
+      template: "./public/index.html",
+      inject: "body"
     })
   ]
 };
