e4cb21ec28768fcc5e1772fa362c0fb6dfe44303
Mpho Matli <mphojames9@gmail.com>
2026-06-15 17:34:55 +0200

    chore: add all remaining backend dependencies

--- frontEnd/src/api/client.js
+++ frontEnd/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const API_URL = 'http://localhost:8000/api';
+//http://localhost:8000/api';
+const API_URL = 'https://remopdf-backend.onrender.com/api';
 
 const downloadBlob = (blob, filename) => {
   const url = window.URL.createObjectURL(blob);
