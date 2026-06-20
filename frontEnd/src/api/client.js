347bfb912c48500e312fe2765cc63db741c3fe02
Mpho Matli <mphojames9@gmail.com>
2026-06-20 12:01:28 +0200

    fix: add missing pdfservices-sdk dependency

--- frontEnd/src/api/client.js
+++ frontEnd/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
-//const API_URL = 'https://remopdf-backend.onrender.com/api';
+//const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://remopdf-backend.onrender.com/api';
 const downloadBlob = (blob, filename) => {
   const url = window.URL.createObjectURL(blob);
   const link = document.createElement('a');
